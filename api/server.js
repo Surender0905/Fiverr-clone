71a591250bf33da0c8d14041f7d6b2271dd4d603
Roshan Singh <Surender@trimir.in>
2023-04-11 16:23:08 +0530

    route set

--- api/server.js
+++ api/server.js
@@ -1,16 +1,41 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+mongoose.set("strictQuery", true);
 
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+/////middleware
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
+
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+
+  return res.status(errorStatus).send(errorMessage);
+});
 
 //////server running on port
 
