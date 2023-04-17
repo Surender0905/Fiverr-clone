f8281b00e2bb626136ee6c89a22150cc706fce5c
Surender Pratap Singh <rk31151@gmail.com>
2023-04-17 08:11:34 +0530

    tested api auth and user

--- api/server.js
+++ api/server.js
@@ -1,19 +1,48 @@
-require('dotenv').config();
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoute from './routes/user.js';
+import gigRoute from './routes/gig.js';
+import orderRoute from './routes/order.js';
+import conversationRoute from './routes/conversation.js';
+import messageRoute from './routes/message.js';
+import reviewRoute from './routes/review.js';
+import authRoute from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
+dotenv.config();
+mongoose.set('strictQuery', true);
 
-// const connect = async () => {
-//   try {
-//     await
-//     console.log()
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('Connected to mongoDB!');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-//connect to server
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
 
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/gigs', gigRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/conversations', conversationRoute);
+app.use('/api/messages', messageRoute);
+app.use('/api/reviews', reviewRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || 'Something went wrong!';
+
+  return res.status(errorStatus).send(errorMessage);
+});
 app.listen(process.env.PORT, () => {
+  connect();
   console.log(`server is running on port ${process.env.PORT}`);
 });
